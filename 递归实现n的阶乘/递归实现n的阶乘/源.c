b43f11d392148ebbc4ea9fa4e2debf78f55a19db
Dai <2524918410@qq.com>
2020-02-29 10:19:30 +0800

    递归实现n的阶乘

---
+++ 递归实现n的阶乘/递归实现n的阶乘/源.c
@@ -0,0 +1,20 @@
+//µÝ¹éÊµÏÖnµÄ½×³Ë
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int Mul(int num)
+{
+	if (num == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return num*Mul(num - 1);
+	}
+
+}
+int main()
+{
+	printf("%d\n", Mul(3));
+	return 0;
+}
